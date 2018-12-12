c658cb00062ebbb72f009bad4bf8f05fca55da0d
ktlee1213 <45408511+ktlee1213@users.noreply.github.com>
2018-12-11 18:19:10 -0600

    Update js-cart-data.js

--- js/js-cart-data.js
+++ js/js-cart-data.js
@@ -2,6 +2,6 @@
 
 var filenames = ["mug.jpg","cap.jpg", "brownies.jpg", "charity-shirt.jpg","giftBasket.jpg", "giftBasket2.jpg"];
 var titles = ["12 OZ mug", "Waterproof Ripstop fabric Cap","Made by YONKERS, brownies mix", "100% Cotton, I HEART Charity Shirt", "Assorted Northen Harvest Gift Basket", "Grill Set Gift Basket"];
-var quantities = [1,1,1,1,1,1];
+var quantities = [1,0,0,0,0,1];
 var prices = [5,10,10,20,50,50];
 
